6d2e27e6eb26d6c3e02bdb2ef051d5a581358607
kevin <web_stark@163.com>
2018-06-06 15:59:50 +0800

    18060601

---
+++ src/components/NewContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-row>
+    姓名：{{info.name}}
+    <el-input v-model="info.name" placeholder="请输入姓名"></el-input>
+    年龄：{{info.age}}
+    <el-input v-model="info.age" placeholder="请输入年龄"></el-input>
+    性别：{{info.sex}}
+    <el-select v-model="info.sex" placeholder="请选择">
+      <el-option v-for="item in options" :key="item" :value="item"></el-option>
+    </el-select>
+  </el-row>
+</template>
+<script>
+    export default {
+        name:"NewContact",
+        data(){
+            return {
+                info: {
+                    name: '',
+                    age: null,
+                    sex: ''
+                },
+                options: [
+                    '女','男','保密'
+                ]
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
